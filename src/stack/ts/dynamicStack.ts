161a9f984267f56e5303880fbb8158c6364aee0c
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-04-27 12:14:17 -0300

    feat: dynamic stack implementation in typescript

---
+++ src/stack/ts/dynamicStack.ts
@@ -0,0 +1,63 @@
+import { INode } from "./types"
+import { EmptyStackError } from "./utils"
+
+export class Node<Type> implements INode<Type> {
+    constructor(
+        public value: Type,
+        public next: INode<Type> | null
+    ) {}
+}
+
+export class DyanmicStack<Type> {
+    private head: INode<Type> | null
+
+    constructor() {
+        this.head = null
+    }
+
+    public push(value: Type): void {
+        if (this.head === null) {
+            this.head = new Node(value, null)
+        } else {
+            this.head = new Node(value, this.head)
+        }
+    }
+
+    public pop(): Type {
+        if (this.head === null) {
+            throw new EmptyStackError()
+        }
+
+        const value = this.head.value
+        this.head = this.head.next
+
+        return value
+    }
+
+    public peek(): Type {
+        if (this.head === null) {
+            throw new EmptyStackError()
+        }
+        return this.head.value
+    }
+
+    public isEmpty(): boolean {
+        return this.head === null
+    }
+
+    public print(): void {
+        if (this.isEmpty()) {
+            throw new EmptyStackError()
+        }
+
+        let output = ""
+        let current = this.head
+
+        while (current !== null) {
+            output += `[ ${current.value} ]\n`
+            current = current.next
+        }
+
+        console.log(output)
+    }
+}
--- src/stack/ts/types.ts
+++ src/stack/ts/types.ts
@@ -2,3 +2,8 @@ export interface StackProps<Type> {
     values: Type[]
     size: number
 }
+
+export interface INode<Type> {
+    value: Type
+    next: INode<Type> | null
+}
