5abfa5b632b696718a8111f1f3db0b08a9f8aa66
nalbert-cerqueira <nalbertc.p@gmail.com>
2024-05-28 19:56:09 -0300

    feat: alternative implementation of quick sort in typescript

--- src/algorithms/sort/ts/quickSort.ts
+++ src/algorithms/sort/ts/quickSort.ts
@@ -1,27 +1,64 @@
-export function quickSort(values: number[]): void {
+export function quickSort1(values: number[]): void {
     if (values.length <= 1) {
         return
     }
-    partition(values, 0, values.length - 1)
+    partition1(values, 0, values.length - 1)
 }
 
-function partition<T>(values: T[], begin: number, end: number): void {
-    if (end <= begin) {
+export function quickSort2(values: number[]): void {
+    if (values.length <= 1) {
+        return
+    }
+    partition2(values, 0, values.length - 1)
+}
+
+function partition1<T>(values: T[], start: number, end: number): void {
+    if (end <= start) {
         return
     }
 
     const pivotIndex = end
-    let i = begin
+    let i = start
 
-    for (let j = begin; j < end; j++) {
+    for (let j = start; j < end; j++) {
         if (values[j] < values[pivotIndex]) {
             swap(values, i, j)
             i++
         }
     }
     swap(values, i, pivotIndex)
-    partition(values, begin, i - 1)
-    partition(values, i + 1, end)
+    partition1(values, start, i - 1)
+    partition1(values, i + 1, end)
+}
+
+function partition2<T>(values: T[], start: number, end: number): void {
+    if (end <= start) {
+        return
+    }
+
+    const ref = values[start]
+    let left = start + 1
+    let right = end
+
+    while (left <= right) {
+        if (values[left] > ref && values[right] < ref) {
+            swap(values, left, right)
+        }
+
+        if (values[left] <= ref) {
+            left += 1
+        }
+
+        if (values[right] >= ref) {
+            right -= 1
+        }
+    }
+
+    const pivotIndex = right
+    swap(values, pivotIndex, start)
+
+    partition2(values, start, pivotIndex - 1)
+    partition2(values, pivotIndex + 1, end)
 }
 
 function swap<T>(values: T[], i: number, j: number): void {
